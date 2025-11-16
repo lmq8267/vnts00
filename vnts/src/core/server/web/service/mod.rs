c5d0f0dcfef3f580038d47fafb1150dbd6d94ab2
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2025-11-17 07:17:47 +0800

    Update mod.rs

--- vnts/src/core/server/web/service/mod.rs
+++ vnts/src/core/server/web/service/mod.rs
@@ -10,6 +10,7 @@ use base64::Engine;
 use crossbeam_utils::atomic::AtomicCell;
 use ipnetwork::Ipv4Network;
 use rsa::rand_core::RngCore;
+use sha2::Digest;
 
 use crate::core::entity::WireGuardConfig;
 
@@ -309,5 +310,12 @@ impl VntsWebService {
         } else {  
             Err(anyhow!("组网不存在"))  
         }  
+    }
+    
+    fn hash_password(password: &str, salt: &str) -> String {  
+        let mut hasher = sha2::Sha256::new();  
+        hasher.update(password.as_bytes());  
+        hasher.update(salt.as_bytes());  
+        format!("{:x}", hasher.finalize())  
     }
 }
